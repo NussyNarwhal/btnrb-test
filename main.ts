277269b1801e6f0e54eb7713b5e0db5c450c1cc7
NussyNarwhal <98988961+NussyNarwhal@users.noreply.github.com>
2022-09-26 10:56:41 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,26 @@
-pins.setPull(DigitalPin.P5, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P11, PinPullMode.PullUp)
 basic.showIcon(IconNames.Yes)
+pins.setPull(DigitalPin.P5, PinPullMode.PullUp)
+basic.showIcon(IconNames.EigthNote)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P5) == 0) {
         pins.digitalWritePin(DigitalPin.P0, 0)
         pins.digitalWritePin(DigitalPin.P5, 1)
-        basic.pause(200)
-        basic.showIcon(IconNames.Surprised)
+        basic.showIcon(IconNames.Meh)
     } else {
         pins.digitalWritePin(DigitalPin.P0, 1)
         pins.digitalWritePin(DigitalPin.P5, 0)
-        basic.pause(200)
+        basic.showIcon(IconNames.Heart)
+    }
+})
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P11) == 0) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P11, 1)
+        basic.showIcon(IconNames.Surprised)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P11, 0)
         basic.showIcon(IconNames.Heart)
     }
 })
